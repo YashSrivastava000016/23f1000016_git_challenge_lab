7f4704e51684878b26d13804051b91a84ec536a8
Yash Srivastava <23f1000016@ds.study.iitm.ac.in>
2025-09-17 00:47:29 +0530

    Add square_root module

---
+++ square_root.py
@@ -0,0 +1,8 @@
+import math
+
+def square_root(x):
+    return math.sqrt(x)
+
+if __name__ == "__main__":
+    num = float(input("Enter a number: "))
+    print(f"Square root of {num} is {square_root(num)}")
